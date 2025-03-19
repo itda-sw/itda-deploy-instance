034d4335dcb434aac9e0ff4ff25895be6b3b3501
itda-sw <itda-sw@itdasemi.com>
2025-03-19 14:50:19 +0000

    update init

--- bash_scripts/aws_exist_nginx_conf.sh
+++ bash_scripts/aws_exist_nginx_conf.sh
@@ -3,5 +3,5 @@ set -e
 
 subdomain=${1}
 
-port=$(grep -oP 'proxy_pass http://localhost:\K[0-9]+' /etc/nginx/sites-available/${subdomain}.soc-canvas.com1)
+port=$(grep -oP 'proxy_pass http://localhost:\K[0-9]+' /etc/nginx/sites-available/${subdomain}.soc-canvas.com)
 echo ${port}
