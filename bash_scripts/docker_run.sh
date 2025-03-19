c8527d02977328fe69953527cec37ce8ba516502
itda-sw <itda-sw@itdasemi.com>
2025-03-19 12:33:52 +0000

    update init

--- bash_scripts/docker_run.sh
+++ bash_scripts/docker_run.sh
@@ -3,8 +3,9 @@ set -e
 
 subdomain=${1}
 docker_image=${2}
+aws_region=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
 
-aws ecr get-login-password | sudo docker login --username AWS --password-stdin ${docker_image}
+aws ecr get-login-password --region ${aws_region} | sudo docker login --username AWS --password-stdin ${docker_image}
 
 sudo docker pull ${docker_image}
 
