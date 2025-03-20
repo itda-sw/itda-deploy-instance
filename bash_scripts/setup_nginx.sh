20c5175763c45fbd2ff6f4ae7f034ae6d2913d18
itda-sw <itda-sw@itdasemi.com>
2025-03-20 15:26:41 +0000

    update

--- bash_scripts/setup_nginx.sh
+++ bash_scripts/setup_nginx.sh
@@ -3,9 +3,9 @@ set -e
 
 subdomain=${1}
 
-if [ -f "/etc/nginx/sites-available/${subdomain}.soc-canvas.com" ]; then
-    echo "파일이 이미 존재합니다. 스킵합니다."
-else
+# if [ -f "/etc/nginx/sites-available/${subdomain}.soc-canvas.com" ]; then
+#     echo "파일이 이미 존재합니다. 스킵합니다."
+# else
   sudo mv /home/ubuntu/${subdomain}.soc-canvas.com /etc/nginx/sites-available/
   sudo rm -rf /etc/nginx/sites-enabled/${subdomain}.soc-canvas.com
   sudo ln -s /etc/nginx/sites-available/${subdomain}.soc-canvas.com /etc/nginx/sites-enabled/ || true
@@ -13,4 +13,4 @@ else
 
   sudo certbot --nginx --email hungry.cho@itdasemi.com --agree-tos --no-eff-email --redirect -d ${subdomain}.soc-canvas.com
   sudo systemctl restart nginx
-fi
+# fi
