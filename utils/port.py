8296db1ee90b7fbef9e80a00b40a17cf7d296b08
itda-sw <itda-sw@itdasemi.com>
2025-03-21 04:48:33 +0000

    update

--- utils/port.py
+++ utils/port.py
@@ -5,9 +5,14 @@ import lib
 script_runner = lib.ScriptRunner(os.path.join("/home/ubuntu/itda-deploy-instance", "bash_scripts"))
 
 def get_port() -> str:
-      start_port = 9050
+  start_port = 9050
 
-      for port in range(start_port, start_port + 1000 - 1):
-        if script_runner.run(f"using_port.sh {port}"):
-            continue
-        return port 
+  for port in range(start_port, start_port + 1000 - 1):
+    if script_runner.run(f"using_port.sh {port}"):
+        continue
+    return port 
+      
+def get_nginx_port(subdomain) -> str:
+    port = script_runner.run(f'nginx_port.sh {subdomain}', True)
+    return port
+    
