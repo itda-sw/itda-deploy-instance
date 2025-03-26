d6f063f2c9c28378f20952e783c11eed933a5813
itda-sw <itda-sw@itdasemi.com>
2025-03-26 02:53:08 +0000

    update

--- bash_scripts/setup_nginx.sh
+++ bash_scripts/setup_nginx.sh
@@ -10,7 +10,4 @@ else
   sudo rm -rf /etc/nginx/sites-enabled/${subdomain}.soc-canvas.com
   sudo ln -s /etc/nginx/sites-available/${subdomain}.soc-canvas.com /etc/nginx/sites-enabled/ || true
   sudo systemctl restart nginx
-
-  sudo certbot --nginx --email hungry.cho@itdasemi.com --agree-tos --no-eff-email --redirect -d ${subdomain}.soc-canvas.com
-  sudo systemctl restart nginx
 fi
