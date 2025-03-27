3788f2ddd526913c0d900bdadcf5d1869e9757ef
itda-sw <itda-sw@itdasemi.com>
2025-03-27 10:31:46 +0000

    update docker img delete

--- bash_scripts/docker_delete.sh
+++ bash_scripts/docker_delete.sh
@@ -3,4 +3,4 @@ set -e
 
 subdomain=${1}
 
-sudo docker images --format "{{.ID}} {{.Repository}} {{.CreatedAt}}" | grep ${subdomain}: | sort -r -k3 | tail -n +2 | awk '{print $1}' | xargs -r sudo docker rmi
+sudo docker images --format "{{.ID}} {{.Repository}}:{{.Tag}} {{.CreatedAt}}" | grep ${subdomain}: | sort -r -k3 | tail -n +2 | awk '{print $1}' | xargs -r sudo docker rmi
