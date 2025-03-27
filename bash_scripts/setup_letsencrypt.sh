f473507a33e96f5f193aef7cf5702ce64f9faea4
itda-sw <itda-sw@itdasemi.com>
2025-03-27 07:12:08 +0000

    update syntax

--- bash_scripts/setup_letsencrypt.sh
+++ bash_scripts/setup_letsencrypt.sh
@@ -3,5 +3,5 @@ set -e
 
 subdomain=${1}
 
-  sudo certbot --nginx --email hungry.cho@itdasemi.com --agree-tos --no-eff-email --redirect -d ${subdomain}.soc-canvas.com
-  sudo systemctl restart nginx
+sudo certbot --nginx --email hungry.cho@itdasemi.com --agree-tos --no-eff-email --redirect -d ${subdomain}.soc-canvas.com
+sudo systemctl restart nginx
